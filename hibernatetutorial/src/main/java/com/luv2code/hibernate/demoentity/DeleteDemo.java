238caf8d9c69d81b4c908b0fb40a24ba60c07be7
tomaes89sv <tomaes89sv@gmail.com>
2020-01-13 13:14:17 +0100

    update and delete added

---
+++ hibernatetutorial/src/main/java/com/luv2code/hibernate/demoentity/DeleteDemo.java
@@ -0,0 +1,32 @@
+package com.luv2code.hibernate.demoentity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try{
+            session.beginTransaction();
+            int studentId = 1;
+            Student myStudent = session.get(Student.class, studentId);
+            session.delete(myStudent);
+            session.getTransaction().commit();
+        }catch (Exception exp) {
+            exp.printStackTrace();
+        }finally {
+            session.close();
+        }
+
+        //pozriet videa od 187
+    }
+
+}
