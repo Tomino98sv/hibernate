01970a1e5a66c376ca0caeec655a907405c9b045
tomaes89sv <tomaes89sv@gmail.com>
2020-02-19 09:33:09 +0100

    some details

--- hibernatetutorial/src/main/java/com/tutorial/hibernate/demo/CreateDemo.java
+++ hibernatetutorial/src/main/java/com/tutorial/hibernate/demo/CreateDemo.java
@@ -2,7 +2,6 @@ package com.tutorial.hibernate.demo;
 
 import com.tutorial.demo.entity.Instructor;
 import com.tutorial.demo.entity.InstructorDetail;
-import com.tutorial.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
